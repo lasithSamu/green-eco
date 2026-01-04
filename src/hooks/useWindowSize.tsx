501c8d617d04e0e864e22699d6f7e25376efd0ed
Lasith <lasithsamuditha9@gmail.com>
2026-01-04 13:56:48 +0530

    Issues fixing

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export function useWindowSize() {
-  const [width, setWidth] = useState<number | undefined>(undefined); // 👈 specify type
+  const [width, setWidth] = useState<number | undefined>(undefined); 
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
