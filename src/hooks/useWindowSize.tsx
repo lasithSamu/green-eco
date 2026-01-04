aae27a252b91df61a10014cc133b81604bafe984
Lasith <lasithsamuditha9@gmail.com>
2026-01-04 14:09:53 +0530

    Mobile menu Padding remove

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export function useWindowSize() {
-  const [width, setWidth] = useState(undefined); 
+  const [width, setWidth] = useState<number | undefined>(undefined); // 👈 specify type
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
